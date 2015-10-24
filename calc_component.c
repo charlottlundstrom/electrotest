d3eb9f5ff2a57f178ff647d8a4859ebcf519b5f6
joakim_strandberg <joakim_strandberg@yahoo.se>
2015-10-24 23:41:51 +0200

    Testar att skicka upp fil

---
+++ calc_component.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* finns 61 värden i serien mellan 10 ohm och 1 megaohm */
+#define E12_MIN 10
+#define E12_MAX 1000000
+/* vilka e12 som finns att välja på och 10 multiplar av dem */
+  int antal;
+  float totRes=83;
+  float resistors[3];
+  float *val_pekare=&resistors[0];
+  const float E12[]={10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82, 100};
+
+int hitta_E12_component(float value, float *res_pointer)
+
+{
+  const float *val_pekare = &E12[0];
+  int tiopotens=1;
+  int hittad = 0;
+
+  *res_pointer=0;
+ if (value > E12_MIN/2){
+    hittad = 1;
+    if (value > E12_MAX)
+      *res_pointer = E12_MAX;
+    else{
+      while (value > 100){
+	value /= 10;
+	tiopotens *= 10;
+      }
+      while (value > *++val_pekare);
+      *res_pointer = *(val_pekare-1) * tiopotens;
+    }
+  }
+  return hittad;
+}
+
+  int e_resistance(float orig_resistance, float *res_array)
+{
+  int i, count=0;
+  float log = 0.001;
+
+  for (i=3; i>0; i--){
+    if (orig_resistance  < 56 )
+      log = 0.1;
+    count += hitta_E12_component(orig_resistance*(1+log), res_array);
+    orig_resistance -= *res_array++;
+    log *=10;
+    }
+
+  return count;
+}
+
+int main() {
+
+  printf("Ersättningsresistanser i E12-serien kopplade i serie: ");
+  for (antal = e_resistance(totRes, &resistors[0]); antal>0; antal--){
+    /* avrundar till närmaste E12 och skriver ut utan decimal*/
+    printf("%1.0f", *val_pekare++);
+    /* behövs fler ersättningsresistanser? */
+    if (antal>1)
+      printf(", ");
+  }
+  printf("\n");
+
+}
+
+
